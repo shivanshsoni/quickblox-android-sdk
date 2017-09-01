0bf433d5f477a9cc3800ac1c8c24cb87d2653ed1
Roman Pronin <roman.pronin@injoit.com>
2017-09-01 11:22:06 +0300

    add supporting Bluetooth headset

--- sample-videochat-webrtc/src/main/java/com/quickblox/sample/groupchatwebrtc/activities/CallActivity.java
+++ sample-videochat-webrtc/src/main/java/com/quickblox/sample/groupchatwebrtc/activities/CallActivity.java
@@ -2,12 +2,19 @@ package com.quickblox.sample.groupchatwebrtc.activities;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.media.projection.MediaProjectionManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
@@ -111,6 +118,13 @@ public class CallActivity extends BaseActivity implements QBRTCClientSessionCall
     private PermissionsChecker checker;
     private MediaProjectionManager mMediaProjectionManager;
 
+    private BluetoothAdapter mBluetoothAdapter;
+    private AudioManager mAudioManager;
+
+    private boolean mIsCountDownOn;
+    private boolean mIsOnHeadsetSco;
+    private boolean mIsStarting;
+
     public static void start(Context context,
                              boolean isIncomingCall) {
 
@@ -140,8 +154,11 @@ public class CallActivity extends BaseActivity implements QBRTCClientSessionCall
 
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
         sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mAudioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
         initQBRTCClient();
         initAudioManager();
+        startBluetooth();
         initWiFiManagerListener();
 
         ringtonePlayer = new RingtonePlayer(this, R.raw.beep);
@@ -293,6 +310,52 @@ public class CallActivity extends BaseActivity implements QBRTCClientSessionCall
             }
         });
         audioManager.init();
+    }
+
+
+    private boolean startBluetooth() {
+        Log.d(TAG, "startBluetooth");
+
+        // Device support bluetooth
+        if (mBluetoothAdapter != null) {
+            if (mAudioManager.isBluetoothScoAvailableOffCall()) {
+                registerReceiver(mBroadcastReceiver,
+                        new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED));
+                registerReceiver(mBroadcastReceiver,
+                        new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECTED));
+                registerReceiver(mBroadcastReceiver,
+                        new IntentFilter(AudioManager.ACTION_SCO_AUDIO_STATE_CHANGED));
+
+                // Need to set audio mode to MODE_IN_CALL for call to startBluetoothSco() to succeed.
+                mAudioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
+
+                mIsCountDownOn = true;
+                // mCountDown repeatedly tries to start bluetooth Sco audio connection.
+                mCountDown.start();
+
+                // need for audio sco, see mBroadcastReceiver
+                mIsStarting = true;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void stopBluetooth() {
+        Log.d(TAG, "stopBluetooth");
+
+        if (mIsCountDownOn) {
+            mIsCountDownOn = false;
+            mCountDown.cancel();
+        }
+
+        // Need to stop Sco audio connection here when the app
+        // change orientation or close with headset still turns on.
+        unregisterReceiver(mBroadcastReceiver);
+        mAudioManager.stopBluetoothSco();
+        mAudioManager.setMode(AudioManager.MODE_NORMAL);
     }
 
     private void setAudioDeviceDelayed(final AppRTCAudioManager.AudioDevice audioDevice) {
@@ -475,6 +538,7 @@ public class CallActivity extends BaseActivity implements QBRTCClientSessionCall
     protected void onStop() {
         super.onStop();
         QBChatService.getInstance().removeConnectionListener(connectionListener);
+        stopBluetooth();
     }
 
 
@@ -858,4 +922,116 @@ public class CallActivity extends BaseActivity implements QBRTCClientSessionCall
             callback.onOpponentsListUpdated(newUsers);
         }
     }
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @SuppressWarnings({"deprecation", "synthetic-access"})
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (action.equals(BluetoothDevice.ACTION_ACL_CONNECTED)) {
+                BluetoothDevice mConnectedHeadset = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                BluetoothClass bluetoothClass = mConnectedHeadset.getBluetoothClass();
+                if (bluetoothClass != null) {
+                    // Check if device is a headset. Besides the 2 below, are there other
+                    // device classes also qualified as headset?
+                    int deviceClass = bluetoothClass.getDeviceClass();
+                    if (deviceClass == BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE
+                            || deviceClass == BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET) {
+                        // start bluetooth Sco audio connection.
+                        // Calling startBluetoothSco() always returns faIL here,
+                        // that why a count down timer is implemented to call
+                        // startBluetoothSco() in the onTick.
+                        mAudioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
+                        mIsCountDownOn = true;
+                        mCountDown.start();
+
+                        // override this if you want to do other thing when the device is connected.
+//                        onHeadsetConnected();
+                    }
+                }
+
+                Log.d(TAG, mConnectedHeadset.getName() + "connected");
+            } else if (action.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED)) {
+                Log.d(TAG, "Headset disconnected");
+
+                if (mIsCountDownOn) {
+                    mIsCountDownOn = false;
+                    mCountDown.cancel();
+                }
+
+                mAudioManager.setMode(AudioManager.MODE_NORMAL);
+
+                // override this if you want to do other thing when the device is disconnected.
+//                onHeadsetDisconnected();
+            } else if (action.equals(AudioManager.ACTION_SCO_AUDIO_STATE_CHANGED)) {
+                int state = intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE,
+                        AudioManager.SCO_AUDIO_STATE_ERROR);
+
+                if (state == AudioManager.SCO_AUDIO_STATE_CONNECTED) {
+                    mIsOnHeadsetSco = true;
+
+                    if (mIsStarting) {
+                        // When the device is connected before the application starts,
+                        // ACTION_ACL_CONNECTED will not be received, so call onHeadsetConnected here
+                        mIsStarting = false;
+//                        onHeadsetConnected();
+                    }
+
+                    if (mIsCountDownOn) {
+                        mIsCountDownOn = false;
+                        mCountDown.cancel();
+                    }
+
+                    // override this if you want to do other thing when Sco audio is connected.
+//                    onScoAudioConnected();
+
+                    Log.d(TAG, "Sco connected");
+                } else if (state == AudioManager.SCO_AUDIO_STATE_DISCONNECTED) {
+                    Log.d(TAG, "Sco disconnected");
+
+                    // Always receive SCO_AUDIO_STATE_DISCONNECTED on call to startBluetooth()
+                    // which at that stage we do not want to do anything. Thus the if condition.
+                    if (!mIsStarting) {
+                        mIsOnHeadsetSco = false;
+
+                        // Need to call stopBluetoothSco(), otherwise startBluetoothSco()
+                        // will not be successful.
+                        mAudioManager.stopBluetoothSco();
+
+                        // override this if you want to do other thing when Sco audio is disconnected.
+//                        onScoAudioDisconnected();
+                    }
+                }
+            }
+        }
+    };
+    /**
+     * Try to connect to audio headset in onTick.
+     */
+    private CountDownTimer mCountDown = new CountDownTimer(10000, 1000) {
+
+        @SuppressWarnings("synthetic-access")
+        @Override
+        public void onTick(long millisUntilFinished) {
+            // When this call is successful, this count down timer will be canceled.
+            try {
+                mAudioManager.startBluetoothSco();
+            } catch (Exception ignored) {
+
+            }
+            Log.d(TAG, "\nonTick start bluetooth Sco");
+        }
+
+        @SuppressWarnings("synthetic-access")
+        @Override
+        public void onFinish() {
+            // Calls to startBluetoothSco() in onStick are not successful.
+            // Should implement something to inform user of this failure
+            mIsCountDownOn = false;
+            mAudioManager.setMode(AudioManager.MODE_NORMAL);
+
+            Log.d(TAG, "\nonFinish fail to connect to headset audio");
+        }
+    };
 }
